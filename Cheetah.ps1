03a36567b3d12b4eff9ccde967e02f2577de1f10
Mike Musa <54551131+mmusa90@users.noreply.github.com>
2019-08-26 21:38:31 -0700

    Update Cheetah.ps1

--- Cheetah.ps1
+++ Cheetah.ps1
@@ -62,14 +62,6 @@ $CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel <#Actio
 $form.CancelButton = $CancelButton <#Create button in form#>
 $form.Controls.Add($CancelButton)
 
-
-$About_ME = New-Object System.Windows.Forms.Button <# Creation Button Design#>
-$About_ME.Location = New-Object System.Drawing.Point(350,150) <# Button Location#>
-$About_ME.Size = New-Object System.Drawing.Size(75,23)<# Button Size #>
-$About_ME.Text = 'Author'<# Label Text for button #>
-$About_ME.add_Click({[system.Diagnostics.Process]::start("https://www.linkedin.com/in/mikemusa/")}) 
-$form.Controls.Add($About_ME)
-
 $label = New-Object System.Windows.Forms.Label <# Creation form label #>
 $label.Location = New-Object System.Drawing.Point(10,20) <# Label location#>
 $label.Size = New-Object System.Drawing.Size(280,20)<# label size#>
@@ -151,9 +143,8 @@ pause
 if ($x.Equals('7-ZIP 2019')){ <#Install 7-ZIP or update it if it has been selected#>
 
 
-#Location of PsExec                           #Server=Workstation  # Command you want to run as an admin 
 
-\\NetworkPath\E\Apps\PSTools\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\NetworkPath\E\Apps\7z\7z.bat #Batch script
+\\dc01\Apps\PsExec\PsExec.exe -i -s -d \\$Server cmd /c start /min \\dc01\Apps\7z\7z.bat
 
 Clear
 
@@ -168,8 +159,7 @@ if ($x.Equals('Adobe Reader 2019')){ <#Install Adobe or update it if it has been
 
 
 
-
-\\NetworkPath\E\Apps\PSTools\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\NetworkPath\E\Apps\adobe\adobe.bat
+\\dc01\Apps\PsExec\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\dc01\Apps\adobe\adobe.bat
 
 Clear
 
@@ -182,7 +172,7 @@ if ($x.Equals('VLC 2019')){ <#Install VLC or update it if it has been selected#>
 
 
 
-\\NetworkPath\E\Apps\PSTools\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\NetworkPath\E\Apps\vlc\vlc.bat
+\\dc01\Apps\PsExec\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\dc01\Apps\vlc\vlc.bat
 
 Clear
 
@@ -193,7 +183,7 @@ pause
 if ($x.Equals('SilverLight 2019')){ <#Install Silverlight or update it if it has been selected#>
 
 
-\\NetworkPath\E\Apps\PSTools\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\NetworkPath\E\Apps\silver\silver.bat
+\\dc01\Apps\PsExec\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\dc01\Apps\silver\silver.bat
 Clear
 
 Write-Host "The" $x "has been sucessfully deployed to" $Server 
@@ -205,7 +195,7 @@ if ($x.Equals('Horizon POS 2019')){ <#Install POS or update it if it has been se
 
 
 
-\\NetworkPath\E\Apps\PSTools\PsExec.exe -i -s -d \\$Server cmd /c start /min \\NetworkPath\E\Apps\POS\Install.bat
+\\dc01\Apps\PsExec\PsExec.exe -i -s -d \\$Server cmd /c start /min \\dc01\Apps\POS\Install.bat
 Clear
 
 Write-Host "The" $x "has been sucessfully deployed to" $Server 
@@ -217,7 +207,7 @@ if ($x.Equals('GP 2018')){ <#Install GP18 or update it if it has been selected#>
 
 
 
-\\NetworkPath\E\Apps\PSTools\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\NetworkPath\E\Apps\GP\install.bat
+\\dc01\Apps\PsExec\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\dc01\Apps\GP\install.bat
 
 clear
 
@@ -230,7 +220,7 @@ if ($x.Equals('Windows Update 1809')){ <#Install Windows Update or update it if 
 
 
 
-\\NetworkPath\E\Apps\PSTools\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\NetworkPath\E\Apps\Windows\Windows\update.bat
+\\dc01\Apps\PsExec\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\dc01\Apps\Windows\Windows\update.bat
 
 clear
 
@@ -243,7 +233,7 @@ if ($x.Equals('Java Update 2019')){ <#Install Java Update or update it if it has
 
 
 
-\\NetworkPath\E\Apps\PSTools\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\NetworkPath\E\Apps\Java\java.bat
+\\dc01\Apps\PsExec\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\dc01\Apps\Java\java.bat
 
 clear
 
@@ -256,7 +246,7 @@ if ($x.Equals('iTunes 2019')){ <#Install iTunes Update or update it if it has be
 
 
 
-\\NetworkPath\E\Apps\PSTools\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\NetworkPath\E\Apps\iTunes\itunes.bat
+\\dc01\Apps\PsExec\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\dc01\Apps\iTunes\itunes.bat
 
 clear
 
@@ -269,7 +259,7 @@ if ($x.Equals('Meadco Script-PCC addon')){ <#Install Meadco Script Update or upd
 
 
 
-\\NetworkPath\E\Apps\PSTools\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\NetworkPath\E\Apps\Meadco-PCC\Meadco.bat
+\\dc01\Apps\PsExec\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\dc01\Apps\Meadco-PCC\Meadco.bat
 
 clear
 
@@ -282,7 +272,7 @@ if ($x.Equals('I25-Fix Badge barcode')){ <#Install I25-Fix Badge barcode Update 
 
 
 
-\\NetworkPath\E\Apps\PSTools\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\NetworkPath\E\Apps\i25\i25.bat
+\\dc01\Apps\PsExec\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\dc01\Apps\i25\i25.bat
 
 clear
 
@@ -293,12 +283,12 @@ pause
 if ($x.Equals('AlwaysOnVPN')){ <#Install AlwaysOnVPN Update or update it if it has been selected#>
 
 
-XCOPY '\\NetworkPath\E$\Apps\vpn' \\$Server\C$\vpn /s /i /Y
+XCOPY '\\wh-deployment\E$\Apps\vpn' \\$Server\C$\vpn /s /i /Y
 
 
 
 
-\\NetworkPath\E\Apps\PSTools\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\$Server\C$\vpn\vpn.bat
+\\dc01\Apps\PsExec\PsExec.exe -i -s -d \\$Server cmd /c start /min  \\$Server\C$\vpn\vpn.bat
 
 clear
 
